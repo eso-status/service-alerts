e0fa0aa5da5ba88049cd555f2647979a7356759c
dov118 <contact@dov118.dev>
2024-08-31 22:48:25 +0200

    Related Feature v2

--- src/formatter/date.formatter.ts
+++ src/formatter/date.formatter.ts
@@ -9,14 +9,17 @@ export default class DateFormatter {
   constructor(private readonly raw: string) {
     this.rawDate = this.getRawDate();
 
+    const dateData: string = this.getDateData();
+    const hourData: string = this.getHourData();
+
     this.dates = [
       moment()
         .utc()
-        .set('years', this.getYear())
-        .set('months', this.getMonth() - 1)
-        .set('date', this.getDay())
-        .set('hours', this.getHour())
-        .set('minutes', this.getMinute())
+        .set('years', Number(dateData.split('.')[0]))
+        .set('months', Number(dateData.split('.')[1]) - 1)
+        .set('date', Number(dateData.split('.')[2]))
+        .set('hours', Number(hourData.split(':')[0]))
+        .set('minutes', Number(hourData.split(':')[1]))
         .set('seconds', 0)
         .set('milliseconds', 0)
         .utcOffset(0),
@@ -30,23 +33,11 @@ export default class DateFormatter {
     return `${this.raw.split(')')[0]})`;
   }
 
-  private getYear(): number {
-    return Number(this.rawDate.split('.')[0]);
+  private getDateData(): string {
+    return this.rawDate.split(' ')[0];
   }
 
-  private getMonth(): number {
-    return Number(this.rawDate.split('.')[1]);
-  }
-
-  private getDay(): number {
-    return Number(this.rawDate.split('.')[2].split(' ')[0]);
-  }
-
-  private getHour(): number {
-    return Number(this.rawDate.split(':')[0].split(' ')[2]);
-  }
-
-  private getMinute(): number {
-    return Number(this.rawDate.split(' UTC')[0].split(':')[1]);
+  private getHourData(): string {
+    return this.rawDate.split(' ')[2];
   }
 }
