7d72d1913c0a31815db9210ac6f9977dbcc564d0
dov118 <contact@dov118.dev>
2024-09-09 10:12:24 +0200

    Related Feature v2

--- test/data/pattern.ts
+++ test/data/pattern.ts
@@ -837,6 +837,282 @@ const pattern: PatternItem[] = [
       },
     ],
   },
+  {
+    url: ServiceAlertsUrl,
+    date: '2024-08-27T14_02_06',
+    file: '2024-08-27T14_02_06.html',
+    rawList: [
+      '2024.08.27 - 14:00 UTC (10:00 EDT) The PTS is currently unavailable while we perform maintenance.',
+      '2024.08.22 - 14:40 UTC (10:40 EDT) The ESO store and account system are currently available.',
+      '2024.08.22 - 13:00 UTC (9:00 EDT) The ESO store and account system are currently unavailable while we perform maintenance.',
+      '2024.08.21 - 11:50 UTC (7:50 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+      '2024.08.21 - 10:00 UTC (6:00 EDT) The North American Xbox megaserver is currently unavailable while we perform maintenance. The European Xbox megaserver is currently unavailable while we perform maintenance. The North American PlayStation® megaserver is currently unavailable while we perform maintenance. The European PlayStation® megaserver is currently unavailable while we perform maintenance.',
+      '2024.08.19 - 17:00 UTC (13:00 EDT) The North American PC/Mac megaserver is currently available.',
+      '2024.08.19 - 16:00 UTC (12:00 EDT) The European PC/Mac megaserver is currently available.',
+      '2024.08.19 - 8:00 UTC (4:00 EDT) The North American PC/Mac megaserver is currently unavailable while we perform maintenance. The European PC/Mac megaserver is currently unavailable while we perform maintenance.',
+      '2024.08.08 - 15:30 UTC (11:30 EDT) The issues related to logging in to the North American and European Xbox megaservers have been resolved at this time.',
+      '2024.08.08 - 14:50 UTC (10:50 EDT) We are currently investigating issues some players are having logging into the North American and European Xbox megaservers.',
+      '2024.08.07 - 15:45 UTC (11:45 EDT) The North American megaservers are currently available. The European megaservers are currently available. The PTS is currently available. The ESO store and account system are currently available.',
+      '2024.08.07 - 8:00 UTC (4:00 EDT) The North American megaservers are currently unavailable while we perform maintenance. The European megaservers are currently unavailable while we perform maintenance. The PTS is currently unavailable while we perform maintenance. The ESO store and account system are currently unavailable while we perform maintenance.',
+      '2024.08.05 - 18:00 UTC (14:00 EDT) The PTS is currently available.',
+      '2024.08.05 - 14:00 UTC (10:00 EDT)) The PTS is currently unavailable while we perform maintenance.',
+      '2024.07.29 - 18:00 UTC (14:00 EDT) The PTS is currently available.',
+      '2024.07.29 - 14:00 UTC (10:00 EDT)) The PTS is currently unavailable while we perform maintenance.',
+      '2024.07.24 - 15:35 UTC (11:35 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+      '2024.07.24 - 10:00 UTC (6:00 EDT) The North American Xbox megaserver is currently unavailable while we perform maintenance. The European Xbox megaserver is currently unavailable while we perform maintenance. The North American PlayStation® megaserver is currently unavailable while we perform maintenance. The European PlayStation® megaserver is currently unavailable while we perform maintenance.',
+      '2024.07.22 - 17:40 UTC (13:40 EDT) The PTS is currently available.',
+      '2024.07.22 - 12:10 UTC (8:10 EDT) The North American PC/Mac megaserver is currently available. The European PC/Mac megaserver is currently available.',
+      '2024.07.22 - 12:00 UTC (8:00 EDT)) The PTS is currently unavailable while we perform maintenance.',
+      '2024.07.22 - 8:00 UTC (4:00 EDT) The North American PC/Mac megaserver is currently unavailable while we perform maintenance. The European PC/Mac megaserver is currently unavailable while we perform maintenance.',
+      '2024.07.19 - 4:20 UTC (12:20 EDT) The Xbox Live™ service interruption has been resolved.',
+      '2024.07.19 - 0:00 UTC (07.18.24 - 20:00 EDT) Xbox Live™ is currently experiencing a service interruption.',
+      '2024.07.15 - 21:00 UTC (17:00 EDT) The PTS is currently available.',
+      '2024.07.15 - 14:00 UTC (10:00 EDT)) The PTS is currently unavailable while we perform maintenance.',
+      '2024.07.10 - 16:00 UTC (12:00 EDT) The issues related to logging in to the North American PlayStation® megaserver have been resolved at this time.',
+      '2024.07.10 - 15:00 UTC (11:00 EDT) We are currently investigating issues some players are having logging into the North American PlayStation® megaserver.',
+      '2024.07.10 - 14:15 UTC (10:15 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+      '2024.07.10 - 08:00 UTC (04:00 EDT) The North American Xbox megaserver is currently unavailable while we perform maintenance. The European Xbox megaserver is currently unavailable while we perform maintenance. The North American PlayStation® megaserver is currently unavailable while we perform maintenance. The European PlayStation® megaserver is currently unavailable while we perform maintenance.',
+      '2024.07.09 - 12:15 UTC (08:15 EDT) The North American PC/Mac megaserver is currently available. The European PC/Mac megaserver is currently available.',
+      '2024.07.09 - 8:00 UTC (04:00 EDT) The North American PC/Mac megaserver is currently unavailable while we perform maintenance. The European PC/Mac megaserver is currently unavailable while we perform maintenance.',
+      '2024.07.08 - 20:00 UTC (16:00 EDT) The PTS is currently available.',
+      '2024.07.08 - 12:00 UTC (8:00 EDT)) The PTS is currently unavailable while we perform maintenance.',
+      '2024.07.03 - 01:20 UTC (2024.07.02 - 21:20 EDT) The Xbox Live™ service interruption has been resolved.',
+      '2024.07.02 - 18:30 UTC (14:00 EDT) Xbox Live™ is currently experiencing a service interruption.',
+      '2024.07.01 - 14:30 UTC (10:30 EDT) The European megaservers are currently available.',
+      '2024.07.01 - 12:00 UTC (08:00 EDT) The North American megaservers are currently available. The ESO store and account system are currently available.',
+      '2024.07.01 - 8:00 UTC (04:00 EDT) The North American megaservers are currently unavailable while we perform maintenance. The European megaservers are currently unavailable while we perform maintenance. The ESO store and account system are currently unavailable while we perform maintenance.',
+      '2024.06.18 - 9:00 UTC (5:00 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+      '2024.06.18 - 2:00 UTC (2024.06.17 - 22:00 EDT) The North American Xbox megaserver is currently unavailable while we perform maintenance. The European Xbox megaserver is currently unavailable while we perform maintenance. The North American PlayStation® megaserver is currently unavailable while we perform maintenance. The European PlayStation® megaserver is currently unavailable while we perform maintenance.',
+      '2024.06.17 - 11:25 UTC (7:25 EDT) The North American PC/Mac megaserver is currently available. The European PC/Mac megaserver is currently available.',
+      '2024.06.17 - 8:00 UTC (04:00 EDT) The North American PC/Mac megaserver is currently unavailable while we perform maintenance. The European PC/Mac megaserver is currently unavailable while we perform maintenance.',
+      '2024.06.11 - 14:50 UTC (10:50 EDT) The PTS is currently available.',
+      '2024.06.11 - 14:00 UTC (10:00 EDT)) The PTS is currently unavailable while we perform maintenance.',
+      '2024.06.05 - 14:30 UTC (10:30 EDT) The ESO store and account system are currently available.',
+      '2024.06.05 - 12:15 UTC (8:15 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+      '2024.06.05 - 12:00 UTC (8:00 EDT) The ESO store and account system are currently unavailable while we perform maintenance.',
+      '2024.06.05 - 10:00 UTC (06:00 EDT) The North American Xbox megaserver is currently unavailable while we perform maintenance. The European Xbox megaserver is currently unavailable while we perform maintenance. The North American PlayStation® megaserver is currently unavailable while we perform maintenance. The European PlayStation® megaserver is currently unavailable while we perform maintenance.',
+      '2024.06.03 - 12:00 UTC (08:00 EDT) The North American PC/Mac megaserver is currently available. The European PC/Mac megaserver is currently available.',
+      '2024.06.03 - 6:00 UTC (02:00 EDT) The North American PC/Mac megaserver is currently unavailable while we perform maintenance. The European PC/Mac megaserver is currently unavailable while we perform maintenance.',
+    ],
+    expected: [
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.27 - 14:00 UTC (10:00 EDT) The PTS is currently unavailable while we perform maintenance.',
+        slug: ServerPcPtsSlug,
+        type: ServerType,
+        support: PcSupport,
+        zone: PtsZone,
+        status: DownStatus,
+        rawSlug: 'PTS',
+        rawDate: '2024.08.27 - 14:00 UTC (10:00 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 27)
+            .set('hours', 14)
+            .set('minutes', 0)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently unavailable',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.22 - 14:40 UTC (10:40 EDT) The ESO store and account system are currently available.',
+        slug: ServiceStoreEsoSlug,
+        type: ServiceType,
+        support: StoreSupport,
+        zone: EsoZone,
+        status: UpStatus,
+        rawSlug: 'ESO store',
+        rawDate: '2024.08.22 - 14:40 UTC (10:40 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 22)
+            .set('hours', 14)
+            .set('minutes', 40)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.22 - 14:40 UTC (10:40 EDT) The ESO store and account system are currently available.',
+        slug: ServiceSystemAccountSlug,
+        type: ServiceType,
+        support: SystemSupport,
+        zone: AccountZone,
+        status: UpStatus,
+        rawSlug: 'account system',
+        rawDate: '2024.08.22 - 14:40 UTC (10:40 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 22)
+            .set('hours', 14)
+            .set('minutes', 40)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.21 - 11:50 UTC (7:50 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+        slug: ServerPsEuSlug,
+        type: ServerType,
+        support: PsSupport,
+        zone: EuZone,
+        status: UpStatus,
+        rawSlug: 'The European PlayStation® megaserver',
+        rawDate: '2024.08.21 - 11:50 UTC (7:50 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 21)
+            .set('hours', 11)
+            .set('minutes', 50)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.21 - 11:50 UTC (7:50 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+        slug: ServerXboxEuSlug,
+        type: ServerType,
+        support: XboxSupport,
+        zone: EuZone,
+        status: UpStatus,
+        rawSlug: 'The European Xbox megaserver',
+        rawDate: '2024.08.21 - 11:50 UTC (7:50 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 21)
+            .set('hours', 11)
+            .set('minutes', 50)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.21 - 11:50 UTC (7:50 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+        slug: ServerPsNaSlug,
+        type: ServerType,
+        support: PsSupport,
+        zone: NaZone,
+        status: UpStatus,
+        rawSlug: 'North American PlayStation® megaserver',
+        rawDate: '2024.08.21 - 11:50 UTC (7:50 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 21)
+            .set('hours', 11)
+            .set('minutes', 50)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.21 - 11:50 UTC (7:50 EDT) The North American Xbox megaserver is currently available. The European Xbox megaserver is currently available. The North American PlayStation® megaserver is currently available. The European PlayStation® megaserver is currently available.',
+        slug: ServerXboxNaSlug,
+        type: ServerType,
+        support: XboxSupport,
+        zone: NaZone,
+        status: UpStatus,
+        rawSlug: 'The North American Xbox megaserver',
+        rawDate: '2024.08.21 - 11:50 UTC (7:50 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 21)
+            .set('hours', 11)
+            .set('minutes', 50)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.19 - 17:00 UTC (13:00 EDT) The North American PC/Mac megaserver is currently available.',
+        slug: ServerPcNaSlug,
+        type: ServerType,
+        support: PcSupport,
+        zone: NaZone,
+        status: UpStatus,
+        rawSlug: 'The North American PC/Mac megaserver',
+        rawDate: '2024.08.19 - 17:00 UTC (13:00 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 19)
+            .set('hours', 17)
+            .set('minutes', 0)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+      {
+        source: ServiceAlertsUrl,
+        raw: '2024.08.19 - 16:00 UTC (12:00 EDT) The European PC/Mac megaserver is currently available.',
+        slug: ServerPcEuSlug,
+        type: ServerType,
+        support: PcSupport,
+        zone: EuZone,
+        status: UpStatus,
+        rawSlug: 'The European PC/Mac megaserver',
+        rawDate: '2024.08.19 - 16:00 UTC (12:00 EDT)',
+        dates: [
+          moment()
+            .utc()
+            .set('years', 2024)
+            .set('months', 8 - 1)
+            .set('date', 19)
+            .set('hours', 16)
+            .set('minutes', 0)
+            .set('seconds', 0)
+            .set('milliseconds', 0)
+            .utcOffset(0),
+        ],
+        rawStatus: 'currently available',
+      },
+    ],
+  },
 ];
 
 export default pattern;
